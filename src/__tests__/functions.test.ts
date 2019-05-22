adaac18b990c5204512472a15534fd52c51c0e7c
Paolo Roth <iamoctod@gmail.com>
2019-05-22 10:10:59 +0200

    chore: drafts tests

--- src/__tests__/functions.test.ts
+++ src/__tests__/functions.test.ts
@@ -1,3 +1,21 @@
-describe(``, () => {
+import * as functions from '../functions';
 
+describe(`Tests functions`, () => {
+  describe(`Math functions`, () => {
+    test(functions.add.name, () => {
+      expect(functions.add(20)(2)).toBe(22);
+    });
+
+    test(functions.divideBy.name, () => {
+      expect(functions.add(20)(2)).toBe(10);
+    });
+
+    test(functions.multiplyBy.name, () => {
+      expect(functions.add(20)(2)).toBe(40);
+    });
+
+    test(functions.subtract.name, () => {
+      expect(functions.add(20)(2)).toBe(18);
+    });
+  });
 });
