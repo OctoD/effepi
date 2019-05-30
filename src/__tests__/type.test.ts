fc56cdfba07a3e84bf3b692d393f2d5d1d9d0c40
Paolo Roth <iamoctod@gmail.com>
2019-05-30 13:28:19 +0200

    feat: adds ofType

--- src/__tests__/type.test.ts
+++ src/__tests__/type.test.ts
@@ -2,22 +2,31 @@ import * as type from '../type';
 import testFunction from './__ignore__/testFunction';
 
 describe(`Type conversion functions`, () => {
+  testFunction(type.ofType, () => {
+    expect(() => type.ofType('boolean')(123, {} as any)).toThrowError();
+    expect(() => type.ofType('number')(123, {} as any)).not.toThrowError();
+  });
+
   testFunction(type.toArray, () => {
-    expect(
-      Array.isArray(
-        type.toArray()(123)
-      )
-    ).toBeTruthy();
+    expect(Array.isArray(type.toArray()(123))).toBeTruthy();
   });
 
   testFunction(type.toDate, () => {
     const date = new Date();
 
     expect(type.toDate()(date.toJSON()) instanceof Date).toBeTruthy();
-    expect(type.toDate()(date.toJSON()).getDate()).toBeTruthy();
+    expect(
+      type
+        .toDate()(date.toJSON())
+        .getDate()
+    ).toBeTruthy();
 
     expect(type.toDate()(date.getTime()) instanceof Date).toBeTruthy();
-    expect(type.toDate()(date.getTime()).getDate()).toBeTruthy();
+    expect(
+      type
+        .toDate()(date.getTime())
+        .getDate()
+    ).toBeTruthy();
   });
 
   testFunction(type.toNumber, () => {
--- src/type.ts
+++ src/type.ts
@@ -1,3 +1,27 @@
+import { Callable } from './pipe';
+import { getTypeOf } from './helpers';
+
+export type TypeCheckerFunction = (value: unknown) => boolean;
+
+export type KnownTypes =
+  | 'bigint'
+  | 'boolean'
+  | 'function'
+  | 'undefined'
+  | 'number'
+  | 'object'
+  | 'string';
+
+export function ofType<T extends KnownTypes>(typeName: T): Callable {
+  return arg => {
+    if (typeof arg === typeName) {
+      return arg as any;
+    }
+
+    throw new TypeError(`Invalid type ${typeof arg}, expected ${typeName}`);
+  };
+}
+
 export function toArray(): <T>(arg: T) => T[] {
   return arg => [arg];
 }
