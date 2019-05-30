d10aefd41e3de41b9ef75daa1982828e85606fac
Paolo Roth <iamoctod@gmail.com>
2019-05-30 12:08:17 +0200

    feat: adds maybe monad

--- src/__tests__/object.test.ts
+++ src/__tests__/object.test.ts
@@ -1,5 +1,7 @@
 import * as object from '../object';
 import testFunction from './__ignore__/testFunction';
+import { pipe } from '../pipe';
+import { useCallValue, put } from '../misc';
 
 describe(`Object functions`, () => {
   testFunction(object.exclude, () => {
@@ -9,7 +11,10 @@ describe(`Object functions`, () => {
       public baz = new Date();
     }
 
-    const result = object.exclude<Test>('foo', 'bar', 'helloworld' as any)({ foo: 1, bar: 2, baz: new Date() }, {} as any);
+    const result = object.exclude<Test>('foo', 'bar', 'helloworld' as any)(
+      { foo: 1, bar: 2, baz: new Date() },
+      {} as any
+    );
 
     expect(() => object.exclude()('' as any, {} as any)).toThrowError();
 
@@ -19,24 +24,50 @@ describe(`Object functions`, () => {
   });
 
   testFunction(object.hasProperty, () => {
-    expect(
-      object.hasProperty('foo')({}, {} as any)
-    ).toBeFalsy();
+    expect(object.hasProperty('foo')({}, {} as any)).toBeFalsy();
 
-    expect(
-      object.hasProperty('foo')({ foo: 100 }, {} as any)
-    ).toBeTruthy();
+    expect(object.hasProperty('foo')({ foo: 100 }, {} as any)).toBeTruthy();
 
+    expect(object.hasProperty('foo')(null, {} as any)).toBeFalsy();
+  });
+
+  testFunction(object.maybe, async () => {
+    const testObject = { foo: { bar: 100 } };
+
+    expect(() => object.maybe('')(123 as any, {} as any)).toThrowError();
+    expect(object.maybe('foo.bar')(testObject, {} as any)).toBe(100);
+    expect(object.maybe('foo.bar.baz')(testObject, {} as any)).toBe(undefined);
+    expect(object.maybe('foo.bar.baz.hello.world')(testObject, {} as any)).toBe(
+      undefined
+    );
+    expect(object.maybe('foo.baz.bar.hello.world')(testObject, {} as any)).toBe(
+      undefined
+    );
     expect(
-      object.hasProperty('foo')(null, {} as any)
-    ).toBeFalsy();
+      object.maybe('foo.baz.bar.hello.world', 123)(testObject, {} as any)
+    ).toBe(123);
+    expect(
+      object.maybe('foo.baz.bar.hello.world', pipe(useCallValue()))(
+        testObject,
+        {} as any
+      )
+    ).toBeUndefined();
+    expect(
+      object.maybe('foo.baz.bar.hello.world', pipe(put(10)))(
+        testObject,
+        {} as any
+      )
+    ).toBe(10);
+    expect(
+      await pipe(useCallValue())
+        .pipe(object.maybe('foo.baz.bar.hello.world', pipe(put(10))))
+        .resolve(testObject)
+    ).toBe(10);
   });
 
   testFunction(object.merge, () => {
     expect(() => object.merge({})(123, {} as any)).toThrowError();
-    expect(
-      object.merge({ foo: 123 })({ bar: 123 }, {} as any)
-    ).toStrictEqual({
+    expect(object.merge({ foo: 123 })({ bar: 123 }, {} as any)).toStrictEqual({
       foo: 123,
       bar: 123,
     });
@@ -49,7 +80,10 @@ describe(`Object functions`, () => {
       public baz = new Date();
     }
 
-    const result = object.pick<Test>('foo', 'bar', 'helloworld' as any)({ foo: 1, bar: 2, baz: new Date() }, {} as any);
+    const result = object.pick<Test>('foo', 'bar', 'helloworld' as any)(
+      { foo: 1, bar: 2, baz: new Date() },
+      {} as any
+    );
 
     expect(() => object.pick()('' as any, {} as any)).toThrowError();
 
--- src/object.ts
+++ src/object.ts
@@ -1,8 +1,10 @@
-import { ExplicitCallable } from './pipe';
-import { throwIfNotObject } from './helpers';
+import { ExplicitCallable, IContext } from './pipe';
+import { throwIfNotObject, isPipe } from './helpers';
 import { isNullOrUndefined } from 'util';
 
-export function exclude<KObject, Keys extends keyof KObject = keyof KObject>(...keys: Keys[]): ExplicitCallable<KObject, Pick<KObject, Keys>> {
+export function exclude<KObject, Keys extends keyof KObject = keyof KObject>(
+  ...keys: Keys[]
+): ExplicitCallable<KObject, Pick<KObject, Keys>> {
   return (arg: any) => {
     throwIfNotObject(`pick`, arg);
 
@@ -17,28 +19,80 @@ export function exclude<KObject, Keys extends keyof KObject = keyof KObject>(...
   };
 }
 
-export function hasProperty(propertyKey: string): ExplicitCallable<unknown, boolean> {
+export function hasProperty(
+  propertyKey: string
+): ExplicitCallable<unknown, boolean> {
   return arg => {
     if (isNullOrUndefined(arg)) {
       return false;
     }
 
     return Object.prototype.hasOwnProperty.call(arg, propertyKey);
-  }
+  };
 }
 
-export function merge<T extends object, K>(target: T): ExplicitCallable<K, K & T> {
+export function maybe<TObject = unknown, TReturn = unknown>(
+  path: string,
+  fallbackValue?: TReturn
+): ExplicitCallable<TObject, TReturn> {
+  const shouldUseFallback = (current: TObject) => {
+    return isNullOrUndefined(current) && !isNullOrUndefined(fallbackValue);
+  };
+
+  const returnFallbackValue = (context: IContext) => {
+    if (!isPipe(fallbackValue)) {
+      return fallbackValue;
+    }
+
+    const isAsync = context.executionFlow === 'async';
+
+    if (isAsync) {
+      return (fallbackValue.resolve(undefined) as unknown) as TReturn;
+    }
+
+    return fallbackValue.resolveSync(undefined) as TReturn;
+  };
+
+  return (arg, context) => {
+    throwIfNotObject('maybe', arg);
+
+    let pathChunks = path.split('.').reverse();
+    let current = arg;
+
+    while (pathChunks.length) {
+      const key = pathChunks.pop();
+
+      if (!current) {
+        break;
+      }
+
+      current = current[key];
+    }
+
+    if (shouldUseFallback(current)) {
+      return returnFallbackValue(context);
+    }
+
+    return (current as unknown) as TReturn;
+  };
+}
+
+export function merge<T extends object, K>(
+  target: T
+): ExplicitCallable<K, K & T> {
   return arg => {
     throwIfNotObject('merge', arg);
 
     return {
       ...arg,
       ...target,
-    }
-  }
+    };
+  };
 }
 
-export function pick<KObject, Keys extends keyof KObject = keyof KObject>(...keys: Keys[]): ExplicitCallable<KObject, Pick<KObject, Keys>> {
+export function pick<KObject, Keys extends keyof KObject = keyof KObject>(
+  ...keys: Keys[]
+): ExplicitCallable<KObject, Pick<KObject, Keys>> {
   return (arg: any) => {
     throwIfNotObject(`pick`, arg);
 
