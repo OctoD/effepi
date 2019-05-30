da22d83f6811699f3c9f7a4c5deed104277aed7e
Paolo Roth <iamoctod@gmail.com>
2019-05-30 12:21:16 +0200

    chore: changes exports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-export * from './array';
-export * from './helpers';
-export * from './logical';
-export * from './math';
-export * from './misc';
-export * from './object';
 export * from './pipe';
-export * from './string';
-export * from './type';
+import * as array from './array';
+import * as helpers from './helpers';
+import * as logical from './logical';
+import * as math from './math';
+import * as misc from './misc';
+import * as object from './object';
+import * as string from './string';
+import * as type from './type';
+
+export { array, helpers, logical, math, misc, object, string, type };
