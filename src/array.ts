9577a9f72bbe903bfa12172d80d661495bda9e8c
Paolo Roth <iamoctod@gmail.com>
2019-06-12 17:01:36 +0200

    chore: adds jsdocs

--- src/array.ts
+++ src/array.ts
@@ -1,6 +1,22 @@
 import { IPipe, IContext, ExplicitCallable, Callable } from './pipe';
 import { throwContextExecutionFlow, throwIfNotArray } from './helpers';
 
+/**
+ * Applies a pipeline to an array of values.
+ * It works only with async pipelines
+ *
+ * ```
+ * const pipe1 = pipe(useCallValue()).pipe(multiplyBy(2)).pipe(add(11));
+ * const pipe2 = pipe(useCallValue()).pipe(applyEach(pipe1));
+ *
+ * pipe2.resolve([10, 20, 30]) // Promise([31, 51, 71])
+ * ```
+ * @export
+ * @template T
+ * @template R
+ * @param {IPipe<T, R>} pipe
+ * @returns {(arg: T[], context: IContext<T[], R>) => Promise<R>[]}
+ */
 export function applyEach<T, R>(pipe: IPipe<T, R>): (arg: T[], context: IContext<T[], R>) => Promise<R>[] {
   return (values, context) => {
     throwContextExecutionFlow('applyEach', context, 'async');
@@ -10,6 +26,22 @@ export function applyEach<T, R>(pipe: IPipe<T, R>): (arg: T[], context: IContext
   };
 }
 
+/**
+ * Applies a pipeline to an array of values.
+ * It works only with sync pipelines
+ *
+ * ```
+ * const pipe1 = pipe(useCallValue()).pipe(multiplyBy(2)).pipe(add(11));
+ * const pipe2 = pipe(useCallValue()).pipe(applyEach(pipe1));
+ *
+ * pipe2.resolveSync([10, 20, 30]) // [31, 51, 71]
+ * ```
+ * @export
+ * @template T
+ * @template R
+ * @param {IPipe<T, R>} pipe
+ * @returns {(arg: T[], context: IContext<T[], R>) => R[]}
+ */
 export function applyEachSync<T, R>(pipe: IPipe<T, R>): (arg: T[], context: IContext<T[], R>) => R[] {
   return (values, context) => {
     throwContextExecutionFlow('applyEachSync', context, 'sync');
@@ -19,6 +51,22 @@ export function applyEachSync<T, R>(pipe: IPipe<T, R>): (arg: T[], context: ICon
   };
 }
 
+/**
+ * Concatenates previous value with another array.
+ *
+ * If the previous value is not an array an error will be thrown
+ *
+ * ```
+ * pipe(useCallValue())
+ *   .pipe(concat([4,5,6]))
+ *   .resolveSync([1,2,3]) // [1,2,3,4,5,6]
+ * ```
+ *
+ * @export
+ * @template T
+ * @param {T[]} newArr
+ * @returns {ExplicitCallable<T[], T[]>}
+ */
 export function concat<T = unknown>(newArr: T[]): ExplicitCallable<T[], T[]> {
   return arr => {
     throwIfNotArray('concat', arr);
@@ -27,8 +75,53 @@ export function concat<T = unknown>(newArr: T[]): ExplicitCallable<T[], T[]> {
   };
 }
 
+/**
+ * Filters the previous value with a given callback. The callback must return a boolean value.
+ *
+ * If the previous value is not an array an error will be thrown
+ *
+ * ```
+ * pipe(useCallValue())
+ *   .pipe(filter(a => a > 2))
+ *   .resolveSync([1,2,3]) // [3]
+ * ```
+ * @export
+ * @template T
+ * @param {(arg: T) => boolean} filterCallback
+ * @returns {ExplicitCallable<T[], T[]>}
+ */
 export function filter<T = unknown>(filterCallback: (arg: T) => boolean): ExplicitCallable<T[], T[]>;
+/**
+ * Filters the previous value with a given callback. The callback must return a boolean value.
+ *
+ * If the previous value is not an array an error will be thrown
+ *
+ * ```
+ * pipe(useCallValue())
+ *   .pipe(filter((a, index) => index === 1))
+ *   .resolveSync([1,2,3]) // [2]
+ * ```
+ * @export
+ * @template T
+ * @param {(arg: T, index: number) => boolean} filterCallback
+ * @returns {ExplicitCallable<T[], T[]>}
+ */
 export function filter<T = unknown>(filterCallback: (arg: T, index: number) => boolean): ExplicitCallable<T[], T[]>;
+/**
+ * Filters the previous value with a given callback. The callback must return a boolean value.
+ *
+ * If the previous value is not an array an error will be thrown
+ *
+ * ```
+ * pipe(useCallValue())
+ *   .pipe(filter((a, index, array) => array.length > 2 && index === 1 && a > 1))
+ *   .resolveSync([1,2,3]) // [2]
+ * ```
+ * @export
+ * @template T
+ * @param {(arg: T, index: number, arr: T[]) => boolean} filterCallback
+ * @returns {ExplicitCallable<T[], unknown[]>}
+ */
 export function filter<T = unknown>(
   filterCallback: (arg: T, index: number, arr: T[]) => boolean
 ): ExplicitCallable<T[], unknown[]> {
@@ -39,6 +132,21 @@ export function filter<T = unknown>(
   };
 }
 
+/**
+ * Filters the previous value with a given value.
+ *
+ * If the previous value is not an array an error will be thrown
+ *
+ * ```
+ * pipe(useCallValue())
+ *   .pipe(filterWith(2))
+ *   .resolveSync([1,2,3,2,3,2]) // [2,2,2]
+ * ```
+ * @export
+ * @template T
+ * @param {T} value
+ * @returns {ExplicitCallable<T[], T[]>}
+ */
 export function filterWith<T>(value: T): ExplicitCallable<T[], T[]> {
   return arr => {
     throwIfNotArray('filterWith', arr);
@@ -47,8 +155,59 @@ export function filterWith<T>(value: T): ExplicitCallable<T[], T[]> {
   };
 }
 
+/**
+ * Finds a value as specified by a find callback. The callback must return `true`.
+ *
+ * If the value is not found, the pipe will return an `undefined`
+ *
+ * If the previous value is not an array an error will be thrown
+ *
+ * ```
+ * pipe(useCallValue())
+ *   .pipe(find((arg: number) => arg === 1))
+ *   .resolveSync([1,2,3,2,3,2]) // 1
+ * ```
+ * @export
+ * @template T
+ * @param {(arg: T) => boolean} callback
+ * @returns {(ExplicitCallable<T[], T | undefined>)}
+ */
 export function find<T>(callback: (arg: T) => boolean): ExplicitCallable<T[], T | undefined>;
+/**
+ * Finds a value as specified by a find callback. The callback must return `true`.
+ *
+ * If the value is not found, the pipe will return an `undefined`
+ *
+ * If the previous value is not an array an error will be thrown
+ *
+ * ```
+ * pipe(useCallValue())
+ *   .pipe(find((arg: number, index) => index === 5))
+ *   .resolveSync([1,2,3,2,3,2000]) // 2000
+ * ```
+ * @export
+ * @template T
+ * @param {(arg: T, index: number) => boolean} callback
+ * @returns {(ExplicitCallable<T[], T | undefined>)}
+ */
 export function find<T>(callback: (arg: T, index: number) => boolean): ExplicitCallable<T[], T | undefined>;
+/**
+ * Finds a value as specified by a find callback. The callback must return `true`.
+ *
+ * If the value is not found, the pipe will return an `undefined`
+ *
+ * If the previous value is not an array an error will be thrown
+ *
+ * ```
+ * pipe(useCallValue())
+ *   .pipe(find((arg: number, index, array) => index > 2 && index < array.length - 1 && arg > 2))
+ *   .resolveSync([1,2,3,2,3,2]) // 3
+ * ```
+ * @export
+ * @template T
+ * @param {(arg: T, index: number, arr: T[]) => boolean} callback
+ * @returns {(ExplicitCallable<T[], T | undefined>)}
+ */
 export function find<T>(callback: (arg: T, index: number, arr: T[]) => boolean): ExplicitCallable<T[], T | undefined> {
   return arr => {
     throwIfNotArray('findExact', arr);
@@ -61,6 +220,23 @@ export function find<T>(callback: (arg: T, index: number, arr: T[]) => boolean):
   };
 }
 
+/**
+ * Finds an exacts value.
+ *
+ * If the value is not found, the pipe will return an `undefined`
+ *
+ * If the previous value is not an array an error will be thrown
+ *
+ * ```
+ * pipe(useCallValue())
+ *   .pipe(find(1))
+ *   .resolveSync([1,2,3,2,3,2]) // 1
+ * ```
+ * @export
+ * @template T
+ * @param {T} value
+ * @returns {(ExplicitCallable<T[], T | undefined>)}
+ */
 export function findExact<T>(value: T): ExplicitCallable<T[], T | undefined> {
   return arr => {
     throwIfNotArray('findExact', arr);
@@ -73,6 +249,20 @@ export function findExact<T>(value: T): ExplicitCallable<T[], T | undefined> {
   };
 }
 
+/**
+ * Joins the previous value with a given char.
+ *
+ * If the previous value is not an array an error will be thrown
+ *
+ * ```
+ * pipe(useCallValue())
+ *   .pipe(join('*'))
+ *   .resolveSync([1,2,3]) // '1*2*3'
+ * ```
+ * @export
+ * @param {string} char
+ * @returns {ExplicitCallable<unknown[], string>}
+ */
 export function join(char: string): ExplicitCallable<unknown[], string> {
   return arr => {
     throwIfNotArray('applyEach', arr);
@@ -81,6 +271,19 @@ export function join(char: string): ExplicitCallable<unknown[], string> {
   };
 }
 
+/**
+ * Returns the length of the previous value.
+ *
+ * If the previous value is not an array an error will be thrown
+ *
+ * ```
+ * pipe(useCallValue())
+ *  .pipe(length())
+ *  .resolveSync([1,2,3]) // 3
+ * ```
+ * @export
+ * @returns {ExplicitCallable<unknown[], number>}
+ */
 export function length(): ExplicitCallable<unknown[], number> {
   return arr => {
     throwIfNotArray('length', arr);
@@ -89,6 +292,21 @@ export function length(): ExplicitCallable<unknown[], number> {
   };
 }
 
+/**
+ * Returns the nth element in the previous value.
+ *
+ * If the previous value is not an array an error will be thrown
+ *
+ * ```
+ * pipe(useCallValue())
+ *   .pipe(nth(3))
+ *   .resolveSync([0, 2, 5, 12, 24]) // 12
+ * ```
+ *
+ * @export
+ * @param {number} index
+ * @returns {Callable}
+ */
 export function nth(index: number): Callable {
   return arg => {
     throwIfNotArray('nth', arg);
@@ -97,6 +315,20 @@ export function nth(index: number): Callable {
   };
 }
 
+/**
+ * Reverses the previous value.
+ *
+ * If the previous value is not an array an error will be thrown
+ *
+ * ```
+ * pipe(useCallValue())
+ *   .pipe(reverse())
+ *   .resolveSync([1,2,3]) // [3,2,1]
+ * ```
+ *
+ * @export
+ * @returns {ExplicitCallable<unknown[], unknown[]>}
+ */
 export function reverse(): ExplicitCallable<unknown[], unknown[]> {
   return arr => {
     throwIfNotArray('reverse', arr);
